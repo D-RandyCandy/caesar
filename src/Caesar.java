66675586ce51668d78b294d9557f3f585494727e
isaacada <danielisaacariwodo9@gmail.com>
2024-11-08 10:41:07 +0100

    anlegen

---
+++ src/Caesar.java
@@ -0,0 +1,56 @@
+public class Caesar {
+    private String Kt (){
+        String Kt="";
+        String gt="";
+        int s="";
+
+
+
+    }
+
+    public void verschlüsseln(){
+
+
+    }
+    public void entschlüsseln(){
+
+
+    }
+    private int getASCII (char pWert){
+        return (int) pWert;
+
+    }
+    private char getChar (int pWert){
+        return (char) pWert;
+
+    }
+    public void setKt (String pWert){
+
+
+    }
+    public void getKt(String) {
+
+
+    }
+    public void setS (int pWert){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
