1fd10aeb45bbea6c6117c7fde10000460a9c158d
isaacada <danielisaacariwodo9@gmail.com>
2024-11-13 11:32:45 +0100

    anlegen

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -1,22 +1,43 @@
-import java.until.Scanner;
 import java.util.Scanner;
 
 public class Verwaltung {
     private Scanner scanner;
-    private Caesar caesar;
-
+    private Caeser caeser;
     public static void main(String[] args){
         new Verwaltung();
+
+
     }
-    public Verwaltung(){
-        ceasar= new Caesar();
-        scanner = new Scanner (system.in);
+    public Verwaltung() {
+        caeser=new Caeser();
+        scanner = new Scanner(System.in);
         while (true) {
-            system.out.println("HAUPTMENÜ ==");
-            system.out.println("[1] Verschlüsseln");
-            system.out.println("")
+            System.out.println("== HAUPTMENÜ ==");
+            System.out.println("[1] Verschlüsseln");
+            System.out.println("[2] Entschlüsseln");
+            System.out.println("[0] Beenden");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            if (option == 1) {
+                System.out.println("Welches Wort soll Verschlüsselt werden?");
+                String optionString = scanner.nextLine();
+                caeser.setKt(optionString);
+                System.out.println("Gib den Schlüssel an");
+                option = scanner.nextInt();
+                caeser.setS(option);
+                caeser.verschlüsseln();
+                System.out.println("Das verschlüsselte Wort ist " + caeser.getGt());
+            } else if (option == 2) {
+                caeser.verschlüsseln();
+                System.out.println("Das entschlüsselte wort ist " + caeser.getKt());
+
+
+            } else if (option == 0) {
+                break;
+            }
+            System.out.println();
         }
+
+
     }
 }
-
-
